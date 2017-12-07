171345d286ced3d41e52c69736cf42cd2da55698
vpaliyX <vpaliy97@gmail.com>
2017-12-06 21:31:32 -0600

    Add tests

---
+++ python/tests/lists/doubly_linked_list.py
@@ -0,0 +1,9 @@
+import unittest
+import src
+
+class DoubleLinkedListTest(unittest.TestCase):
+    def testAddToFront(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python/tests/trees/binary_tree_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class BinarySearchTreeTest(unittest.TestCase):
+    def __init__(self):
+        print "here"
+
+if __name__ == "__main__":
+    unittest.main()
